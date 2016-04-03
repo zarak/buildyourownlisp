696b772ac2387cee772d0cc481a7a1044f4f9a41
Zarak Mahmud <zarak.mahmud@utoronto.ca>
2016-04-03 14:56:38 +0500

    Add parser and language

--- parsing.c
+++ parsing.c
@@ -24,6 +24,22 @@ void add_history(char *unused) {}
 
 int main(int argc, char *argv[])
 {
+    /* Create some parsers */
+    mpc_parser_t *Number = mpc_new("number");
+    mpc_parser_t *Operator = mpc_new("operator");
+    mpc_parser_t *Expr = mpc_new("expr");
+    mpc_parser_t *Lispy = mpc_new("lispy");
+
+    /* Define them all with the following language */
+    mpca_lang(MPCA_LANG_DEFAULT,
+            "                                                     \
+            number    : /-?[0-9]+/ ;                              \
+            operator  : '+' | '-' | '*' | '/' ;                   \
+            expr      : <number> | '(' <operator> <expr>+ ')' ;   \
+            lispy     : /^/ <operator> <expr>+ /$/ ;              \
+            ",
+            Number, Operator, Expr, Lispy);
+
     /* Print version and exit information */
     puts("Lispy Version 0.0.0.0.1");
     puts("Press Ctrl-c to Exit\n");
@@ -40,5 +56,6 @@ int main(int argc, char *argv[])
         free(input);
     }
 
+    mpc_cleanup(4, Number, Operator, Expr, Lispy);
     return 0;
 }
